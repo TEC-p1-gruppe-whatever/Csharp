b101ff0eb72c1c7b1c5aa300cfef12c72c7539dc
Mandenln <64834066+Mandenln@users.noreply.github.com>
2020-05-11 10:58:54 +0200

    Add files via upload

---
+++ Program/Program/Bil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Bil
+    {
+        public void MotorStart()
+        {
+            Console.WriteLine("Motoren er started");
+        }
+        public void MotorSluk()
+        {
+            Console.WriteLine("Motoren er slukket.");
+        }
+        public double FillGas(double Liters, bool IsDiesel)
+        {
+            Console.Write("Filled tank with {0} Liters of ", Liters);
+            if (IsDiesel)
+            {
+                Console.WriteLine("Diesel.");
+            }
+            else
+            {
+                Console.WriteLine("Gas.");
+            }
+            return Liters;
+        }
+        public double RemainingGas()
+        {
+            double Gas = 4;
+            Console.WriteLine(Gas);
+            return Gas;
+        }
+    }
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bil Mitsubishi = new Bil(); // Opreter genvej til klassen "Bil"
+            Mitsubishi.MotorStart(); // Koere den specificerede metode i klassen "Bil"
+            Mitsubishi.MotorSluk(); // Koere den specificerede metode i klassen "Bil"
+            Mitsubishi.FillGas(2.5, true);
+            double Gas = Mitsubishi.RemainingGas();
+            Console.WriteLine(Gas);
+
+
+
+
+
+            System.Threading.Thread.Sleep(5000); // Pausere programmet, for at give brugeren tid til at laese, for programmet slutter.
+        }
+    }
+}
